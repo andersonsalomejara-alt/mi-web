e931d42ca75910941d6011ba606f7cccbbb1af9d
ANDERSON <andersonsalomejara@gmail.com>
2026-01-23 16:39:44 -0500

    Limpieza de codigo y optimizacion

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-// script.js
+const yearSpan = document.getElementById('year');
 
-// 1. Seleccionamos el botón por su ID (como un puntero al objeto)
-const boton = document.getElementById('boton-tema');
-
-// 2. Definimos la función
 function alternarModo() {
     document.body.classList.toggle('modo-oscuro');
-    
-    // Lógica extra: Cambiar el texto del botón según el estado
     if (document.body.classList.contains('modo-oscuro')) {
         boton.innerText = "Desactivar Modo Oscuro";
     } else {
@@ -15,9 +9,5 @@ function alternarModo() {
     }
 }
 
-// 3. Añadimos el "Event Listener"
-// Cuando el 'boton' reciba un 'click', ejecuta 'alternarModo'
 boton.addEventListener('click', alternarModo);
-const yearSpan = document.getElementById('year');
-const currentYear = new Date().getFullYear();
-yearSpan.innerText = currentYear;
+yearSpan.innerText = new Date().getFullYear();
