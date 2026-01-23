bff50361f0116bcde3b16245ce06c739925b905e
ANDERSON <andersonsalomejara@gmail.com>
2026-01-23 16:43:59 -0500

    Eliminado modo oscuro permanentemente

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 const yearSpan = document.getElementById('year');
-
-function alternarModo() {
-    document.body.classList.toggle('modo-oscuro');
-    if (document.body.classList.contains('modo-oscuro')) {
-        boton.innerText = "Desactivar Modo Oscuro";
-    } else {
-        boton.innerText = "Activar Modo Oscuro";
-    }
-}
-
-boton.addEventListener('click', alternarModo);
-yearSpan.innerText = new Date().getFullYear();
+const currentYear = new Date().getFullYear();
+yearSpan.innerText = currentYear;
